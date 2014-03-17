91df3d1cae8510d8e80cc2ddccd104b8ba399eef
Aaron Holmes <aaron@aaronholmes.net>
2014-03-16 17:03:18 -0700

    Initial commit, which includes window and window-less screencapturing of the current screen.

---
+++ ScreenShot/EntryPoint.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Drawing.Imaging;
+
+namespace ScreenShot
+{
+	// http://stackoverflow.com/a/6156665/1801382
+	public class EntryPoint
+	{
+		[STAThread]
+		public static void Main(string[] args)
+		{
+			if (args != null && args.Length > 0)
+			{
+				foreach (var arg in args)
+				{
+					switch (arg)
+					{
+						case "-noWindow":
+							CaptureNow();
+						return;
+					}
+				}
+			}
+
+			StartMainWindow();
+		}
+
+		public static void StartMainWindow()
+		{
+			var app = new App();
+			var mainWindow = new MainWindow();
+			app.Run(mainWindow);
+		}
+
+		public static void CaptureNow()
+		{
+			var filepath = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+			var filename = "Capture-%NOW%.png";
+
+			ScreenCapturer.CaptureAndSave(filepath + "\\" + filename, CaptureMode.Screen, ImageFormat.Png);
+		}
+
+	}
+}
---
+++ ScreenShot/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing.Imaging;
+using System.Windows;
+
+namespace ScreenShot
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void CaptureScreenshot(object sender, RoutedEventArgs e)
+		{
+			var filepath = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+			var filename = "Capture-%NOW%.png";
+
+			ScreenCapturer.CaptureAndSave(filepath + "\\" + filename , CaptureMode.Screen, ImageFormat.Png);
+		}
+	}
+}
