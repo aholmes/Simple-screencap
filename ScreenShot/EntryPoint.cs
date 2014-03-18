eec9b9ec595d0eb2e400b2a995034a358d235c22
Eric Rodewald <eric.rode@gmail.com>
2014-03-18 12:19:57 -0500

    EntryPoint.cs supports more cmd line args
    -noWindow (unchanged)
    -jpg / -jpeg / -bmp / -png / -tiff
    -screen / -window / -desktop

--- ScreenShot/EntryPoint.cs
+++ ScreenShot/EntryPoint.cs
@@ -1,44 +1,75 @@
 ﻿using System;
 using System.Drawing.Imaging;
+using System.IO;
 
-namespace ScreenShot
-{
-	// http://stackoverflow.com/a/6156665/1801382
-	public class EntryPoint
-	{
-		[STAThread]
-		public static void Main(string[] args)
-		{
-			if (args != null && args.Length > 0)
-			{
-				foreach (var arg in args)
-				{
-					switch (arg)
-					{
-						case "-noWindow":
-							CaptureNow();
-						return;
-					}
-				}
-			}
+namespace ScreenShot {
+  // http://stackoverflow.com/a/6156665/1801382
+  public class EntryPoint {
+    private const string Filename = "Capture-%NOW%";
 
-			StartMainWindow();
-		}
+    private static ImageFormat _format = ImageFormat.Png;
+    private static CaptureMode _mode = CaptureMode.Screen;
+    private static bool _noWindow;
 
-		public static void StartMainWindow()
-		{
-			var app = new App();
-			var mainWindow = new MainWindow();
-			app.Run(mainWindow);
-		}
+    [STAThread]
+    public static void Main(string[] args) 
+    {
+      if (args != null && args.Length > 0) 
+      {
+        foreach (var arg in args) 
+        {
+          switch (arg) 
+          {
+            case "-noWindow":
+              _noWindow = true;
+              break;
+            case "-jpg":
+            case "-jpeg":
+              _format = ImageFormat.Jpeg;
+              break;
+            case "-bmp":
+              _format = ImageFormat.Bmp;
+              break;
+            case "-png":
+              _format = ImageFormat.Png;
+              break;
+            case "-screen":
+              _mode = CaptureMode.Screen;
+              break;
+            case "-window":
+              _mode= CaptureMode.Window;
+              break;
+            case "-desktop":
+              _mode= CaptureMode.Desktop;
+              break;
+          }
+        }
+        if (_noWindow) 
+        {
+          CaptureNow();
+          return;
+        }
+      }
 
-		public static void CaptureNow()
-		{
-			var filepath = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
-			var filename = "Capture-%NOW%.png";
+      StartMainWindow();
+    }
 
-			ScreenCapturer.CaptureAndSave(filepath + "\\" + filename, CaptureMode.Screen, ImageFormat.Png);
-		}
+    public static void StartMainWindow() 
+    {
+      var app = new App();
+      app.InitializeComponent();
+      var mainWindow = new MainWindow();
+      app.Run(mainWindow);
+    }
 
-	}
+    public static void CaptureNow(string basePath = null) 
+    {
+      string filepath;
+      if (basePath == null || !Directory.Exists(basePath))
+        filepath = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+      else filepath = basePath;
+
+      ScreenCapturer.CaptureAndSave(filepath + "\\" + Filename, _mode, _format);
+    }
+  }
 }
